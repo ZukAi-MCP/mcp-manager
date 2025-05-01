3cb56d9a76ec585613beee23f78975f3634ce6f5
Zuk.Ai <3107593460@qq.com>
2025-05-01 19:10:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import electron from "vite-plugin-electron"
 import renderer from "vite-plugin-electron-renderer"
-
+ 
 export default defineConfig({
 	plugins: [
 		react(),
